f61440406d81c944348bf210cb7b5d06893178dd
Isah Idris <idris.isah2@st.futminna.edu.ng>
2026-06-10 15:04:24 +0100

    data-check: added row counts and price range checks

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data):
+    """
+    Test the number of rows in the data set and ensure
+    the data is not too small nor too large
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test the price is in a reasonable range
+    """
+    assert data['price'].between(min_price, max_price).all()
